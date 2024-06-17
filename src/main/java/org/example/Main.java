eff85b7b263a885ea5d3572a65377685f7ec502e
Ugnytė <ugne.kecioryte@gmail.com>
2024-06-17 14:45:07 +0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,167 @@
+package org.example;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static WebDriver browser;
+    public static int counter;
+    public static final String URL = "http://localhost:8080/skaiciuotuvas";
+    public static String innerUrl;
+
+    private static final String COUNTER_FILE = "counter.txt";
+    public static void main(String[] args) {
+        System.out.println("Selenium + Maven + TestNG -> Skaiciuotuvas");
+
+        //        SetUp
+        setUp(URL);
+
+        // Paimti counter reikšmę iš failo ir sukurti unikalu username
+        counter = readCounter();
+        String username = "Testavimas" + counter;
+
+//        •	Naujo vartotojo registracija (pozityvus ir negatyvus testas)
+        //Paspaudžiame "Sukurti naują paskyrą"
+        clickOnElement(By.xpath("/html/body/div/form/div/h4/a"));
+        //Užpildome reikalingus laukus
+        sendKeysToInputField(By.id("username"), username);
+        sendKeysToInputField(By.id("password"), "testavimas");
+        sendKeysToInputField(By.id("passwordConfirm"), "testavimas");
+        //paspaudziame "Sukurti"
+        clickOnElement(By.xpath("//*[@id=\"userForm\"]/button"));
+        //Tikriname ar sėkmingai prisijungėme. Matomas pasirinkimas Logout ir atvaizduotas prisijungusio vartotojo userName
+        boolean containsLogout = elementTextContainsString(By.xpath("/html/body/nav/div/ul[2]/a"), "Logout");
+        boolean containsUsername = elementTextContainsString(By.xpath("/html/body/nav/div/ul[2]/a"), username);
+        System.out.println(containsLogout);
+        System.out.println(containsUsername);
+
+//        •	Naujo įrašo sukūrimas (pozityvus ir negatyvus testas)
+        sendKeysToInputField(By.id("sk1"), "999");
+        sendKeysToInputField(By.id("sk2"), "998");
+        selectValueFromDropdownList(By.xpath("//*[@id=\"number\"]/select"), "-");
+        clickOnElement(By.xpath("//*[@id=\"number\"]/input[3]"));
+        System.out.println(elementTextContainsString(By.tagName("h1"), "Whitelabel Error Page"));
+
+//        •	Esamo įrašo paieška (pozityvus ir negatyvus testas)
+        browser.get(URL);
+        clickOnElement(By.xpath("/html/body/nav/div/ul[1]/li/a"));
+        System.out.println(findRecordInTable(999, 998, "-", 1));
+
+//        •	Esamo įrašo redagavimas (pozityvus ir negatyvus testas)
+        //Papsaudziame redaguoti ant paskutinio iraso
+        clickOnElement(By.xpath("/html/body/div/table/tbody/tr[last()]/td[5]/a[1]"));
+        //Suvedame reiksmes i laukus
+        sendKeysToInputField(By.xpath("//*[@id=\"command\"]/p[1]/input"), "1");
+        sendKeysToInputField(By.xpath("//*[@id=\"command\"]/p[3]/input"), "991");
+        sendKeysToInputField(By.xpath("//*[@id=\"command\"]/p[4]/input"), "-990");
+        //Spaudziame Atnaujinti
+        clickOnElement(By.xpath("//*[@id=\"command\"]/p[5]/input"));
+        //Patikriname ar gerai susivede
+        System.out.println(elementTextContainsString(By.xpath("/html/body/div/table/tbody/tr[2]/td[2]"), "1"));
+        System.out.println(elementTextContainsString(By.xpath("/html/body/div/table/tbody/tr[4]/td[2]"), "991"));
+        System.out.println(elementTextContainsString(By.xpath("/html/body/div/table/tbody/tr[5]/td[2]"), "-990"));
+
+//        •	Esamo įrašo ištrynimas (pozityvus ir negatyvus testas)
+        clickOnElement(By.xpath("/html/body/a"));
+        //Papsaudziame trinti ant paskutinio iraso
+        clickOnElement(By.xpath("/html/body/div/table/tbody/tr[last()]/td[5]/a[2]"));
+        // Pereiname prie alert ir patvirtiname jį
+        Alert alert = browser.switchTo().alert();
+        alert.accept();
+        //Patikriname, kad irasas istrintas - neberandame jo registre
+        System.out.println(findRecordInTable(1, 991, "-", -990));
+
+//        •	Tik autorizuoti vartotojai gali naudotis sistema (pozityvus ir negatyvus testas)
+        clickOnElement(By.xpath("/html/body/nav/div/ul[1]/li/a"));
+        System.out.println(browser.getCurrentUrl());
+        innerUrl = browser.getCurrentUrl();
+        clickOnElement(By.xpath("/html/body/nav/div/ul[2]/a"));
+        browser.get(innerUrl);
+
+        // Padidinti vienu ir išsaugoti skaitiklį counter
+        saveCounter(counter + 1);
+
+        closeBrowser();
+    }
+    public static void setUp(String url){
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "src/drivers/chromedriver125.exe"
+        );
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments(
+                "--start-maximized",
+                "--ignore-certificate-errors"
+        );
+
+        browser = new ChromeDriver(chromeOptions);
+        browser.get(url);
+    }
+    public static void closeBrowser(){
+        browser.quit();
+    }
+    public static void clickOnElement(By locator){
+        browser.findElement(locator).click();
+    }
+    public static void sendKeysToInputField(By locator, String value){
+        WebElement element = browser.findElement(locator);
+        element.clear();
+        element.sendKeys(value);
+    }
+    public static int readCounter() {
+        try (BufferedReader br = new BufferedReader(new FileReader(COUNTER_FILE))) {
+            return Integer.parseInt(br.readLine().trim());
+        } catch (IOException e) {
+            System.out.println("Nepavyko išsaugoti skaitliuko. Plačiau:" + e.getMessage());
+            return 1; // Return a default value in case of error
+        }
+    }
+    public static void saveCounter(int counter) {
+        try (FileWriter fw = new FileWriter(COUNTER_FILE)) {
+            fw.write(Integer.toString(counter));
+        } catch (IOException e) {
+            System.out.println("Nepavyko išsaugoti skaitliuko. Plačiau:" + e.getMessage());
+        }
+    }
+    public static boolean elementTextContainsString(By locator, String word){
+        String logoutText = browser.findElement(locator).getText();
+        return logoutText.contains(word);
+    }
+    public static void selectValueFromDropdownList(By locator, String value){
+        WebElement selectField = browser.findElement(locator);
+        Select select = new Select(selectField);
+        select.selectByValue(value);
+    }
+    public static boolean findRecordInTable(int firstNumber, int secondNumber, String operator, int result) {
+        List<WebElement> rows = browser.findElements(By.cssSelector("table tbody tr"));
+        for (WebElement row : rows) {
+            List<WebElement> cells = row.findElements(By.tagName("td"));
+            if (cells.size() < 4) {
+                // Skip this row if it does not have at least 4 cells
+                continue;
+            }
+
+            int cellFirstNumber = Integer.parseInt(cells.get(0).getText());
+            String cellOperator = cells.get(1).getText();
+            int cellSecondNumber = Integer.parseInt(cells.get(2).getText());
+            int cellResult = Integer.parseInt(cells.get(3).getText());
+
+            if (cellFirstNumber == firstNumber && cellOperator.equals(operator) && cellSecondNumber == secondNumber && cellResult == result) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/CalculatorTestCase.java
@@ -0,0 +1,219 @@
+import org.example.Main;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class CalculatorTestCase {
+
+    @BeforeClass
+    public static  void setUp(){
+        Main.setUp(Main.URL);
+    }
+    @Test (priority = 1)
+    static void registruotiNaujaVartotojaNegatyvusTestas(){
+        //Paspaudžiame "Sukurti naują paskyrą"
+        Main.clickOnElement(By.xpath("/html/body/div/form/div/h4/a"));
+        //Užpildome reikalingus laukus
+        Main.sendKeysToInputField(By.id("username"), "?");
+        Main.sendKeysToInputField(By.id("password"), "a");
+        //paspaudziame "Sukurti"
+        Main.clickOnElement(By.xpath("//*[@id=\"userForm\"]/button"));
+        //Tikriname klaidų pranešimus.
+        boolean usernameErrors = Main.elementTextContainsString(By.id("username.errors"), "Privaloma įvesti nuo 3 iki 32 simbolių");
+        boolean passwordErrors = Main.elementTextContainsString(By.id("password.errors"), "Privaloma įvesti bent 3 simbolius");
+        boolean passwordConfirmErrors = Main.elementTextContainsString(By.id("passwordConfirm.errors"), "Įvesti slaptažodžiai nesutampa");
+        Assert.assertTrue(usernameErrors);
+        Assert.assertTrue(passwordErrors);
+        Assert.assertTrue(passwordConfirmErrors);
+    }
+    @Test(priority = 2)
+    static void registruotiNaujaVartotoja(){
+        // Paimti counter reikšmę iš failo ir sukurti unikalu username
+        Main.counter = Main.readCounter();
+        String username = "Testavimas" + Main.counter;
+
+        //Užpildome reikalingus laukus teisingai
+        Main.sendKeysToInputField(By.id("username"), username);
+        Main.sendKeysToInputField(By.id("password"), "testavimas");
+        Main.sendKeysToInputField(By.id("passwordConfirm"), "testavimas");
+        //paspaudziame "Sukurti"
+        Main.clickOnElement(By.xpath("//*[@id=\"userForm\"]/button"));
+        //Tikriname ar sėkmingai prisijungėme. Matomas pasirinkimas Logout ir atvaizduotas prisijungusio vartotojo userName
+        boolean containsLogout = Main.elementTextContainsString(By.xpath("/html/body/nav/div/ul[2]/a"), "Logout");
+        boolean containsUsername = Main.elementTextContainsString(By.xpath("/html/body/nav/div/ul[2]/a"), username);
+        Assert.assertTrue(containsLogout);
+        Assert.assertTrue(containsUsername);
+    }
+    @Test (priority = 3)
+    static void prisijungtiSuEsamuVartotojuNegatyvusTestas(){
+        //Atsijungiame
+        Main.clickOnElement(By.xpath("/html/body/nav/div/ul[2]/a"));
+
+        //Užpildome reikalingus laukus su esamo vartotojo duomenimis
+        Main.sendKeysToInputField(By.xpath("/html/body/div/form/div/input[1]"), "");
+        Main.sendKeysToInputField(By.xpath("/html/body/div/form/div/input[2]"), "testavimas");
+        //Paspaudziama "Prisijungti"
+        Main.clickOnElement(By.xpath("/html/body/div/form/div/button"));
+        //Tikriname ar sėkmingai prisijungėme. Matomas pasirinkimas Logout ir atvaizduotas prisijungusio vartotojo userName
+        boolean loginErrors = Main.elementTextContainsString(By.xpath("/html/body/div/form/div/span[2]"), "Įvestas prisijungimo vardas ir/ arba slaptažodis yra neteisingi");
+        Assert.assertTrue(loginErrors);
+    }
+    @Test (priority = 4)
+    static void prisijungtiSuEsamuVartotoju(){
+        //Užpildome reikalingus laukus su esamo vartotojo duomenimis
+        Main.sendKeysToInputField(By.xpath("/html/body/div/form/div/input[1]"), "Testavimas" + Main.counter);
+        Main.sendKeysToInputField(By.xpath("/html/body/div/form/div/input[2]"), "testavimas");
+        //Paspaudziama "Prisijungti"
+        Main.clickOnElement(By.xpath("/html/body/div/form/div/button"));
+        //Tikriname ar sėkmingai prisijungėme. Matomas pasirinkimas Logout ir atvaizduotas prisijungusio vartotojo userName
+        boolean containsLogout = Main.elementTextContainsString(By.xpath("/html/body/nav/div/ul[2]/a"), "Logout");
+        boolean containsUsername = Main.elementTextContainsString(By.xpath("/html/body/nav/div/ul[2]/a"), "Testavimas" + Main.counter);
+        Assert.assertTrue(containsLogout);
+        Assert.assertTrue(containsUsername);
+    }
+    @Test (priority = 5)
+    static void kurtiNaujaIrasaNegatyvusTestas() {
+        //Suvedame skaicius
+        Main.sendKeysToInputField(By.id("sk1"), "-2");
+        Main.sendKeysToInputField(By.id("sk2"), "-3");
+        //Pasirenkame veiksma
+        Main.selectValueFromDropdownList(By.xpath("//*[@id=\"number\"]/select"), "+");
+        Main.clickOnElement(By.xpath("//*[@id=\"number\"]/input[3]"));
+        //Tikriname pranesima
+        boolean sk1Error = Main.elementTextContainsString(By.id("sk1.errors"), "Validacijos klaida: skaičius negali būti neigiamas");
+        boolean sk2Error = Main.elementTextContainsString(By.id("sk2.errors"), "Validacijos klaida: skaičius negali būti neigiamas");
+        Assert.assertTrue(sk1Error);
+        Assert.assertTrue(sk2Error);
+    }
+    @Test (priority = 6)
+    static void kurtiNaujaIrasa(){
+        //Suvedame skaicius
+        Main.sendKeysToInputField(By.id("sk1"), "999");
+        Main.sendKeysToInputField(By.id("sk2"), "998");
+        //Pasirenkame veiksma
+        Main.selectValueFromDropdownList(By.xpath("//*[@id=\"number\"]/select"), "-");
+        Main.clickOnElement(By.xpath("//*[@id=\"number\"]/input[3]"));
+        //Tikriname pranesima
+        boolean resultActual = Main.elementTextContainsString(By.tagName("h1"), "Whitelabel Error Page");
+        Assert.assertTrue(resultActual);
+    }
+    @Test (priority = 7)
+    static void ieskotiIrasoRegistre(){
+        Main.browser.get(Main.URL);
+        //Nueiname i irasu registra
+        Main.clickOnElement(By.xpath("/html/body/nav/div/ul[1]/li/a"));
+        //Ieskome musu sukurto iraso
+        boolean resultActual = Main.findRecordInTable(999, 998, "-", 1);
+        Assert.assertTrue(resultActual);
+    }
+    @Test (priority = 8)
+    static void ieskotiIrasoRegistreNegatyvusTestas(){
+        //Ieskome iraso, kuris negali buti sukurtas
+        boolean resultActual = Main.findRecordInTable(-999, 999, "+", 0);
+        Assert.assertFalse(resultActual);
+    }
+    @Test (priority = 9)
+    static void koreguotiIrasa(){
+        //Papsaudziame redaguoti ant paskutinio iraso
+        Main.clickOnElement(By.xpath("/html/body/div/table/tbody/tr[last()]/td[5]/a[1]"));
+        //Suvedame reiksmes i laukus
+        Main.sendKeysToInputField(By.xpath("//*[@id=\"command\"]/p[1]/input"), "1");
+        Main.sendKeysToInputField(By.xpath("//*[@id=\"command\"]/p[3]/input"), "991");
+        Main.sendKeysToInputField(By.xpath("//*[@id=\"command\"]/p[4]/input"), "-990");
+        //Spaudziame Atnaujinti
+        Main.clickOnElement(By.xpath("//*[@id=\"command\"]/p[5]/input"));
+        //Patikriname ar gerai susivede
+        boolean sk1ResultActual = Main.elementTextContainsString(By.xpath("/html/body/div/table/tbody/tr[2]/td[2]"), "1");
+        boolean sk2ResultActual = Main.elementTextContainsString(By.xpath("/html/body/div/table/tbody/tr[4]/td[2]"), "991");
+        boolean rezResultActual =Main.elementTextContainsString(By.xpath("/html/body/div/table/tbody/tr[5]/td[2]"), "-990");
+        Assert.assertTrue(sk1ResultActual);
+        Assert.assertTrue(sk2ResultActual);
+        Assert.assertTrue(rezResultActual);
+    }
+    @Test (priority = 10)
+    static void koreguotiIrasaNegatyvusTestas(){
+        Main.clickOnElement(By.xpath("/html/body/a"));
+
+        //Papsaudziame redaguoti ant paskutinio iraso
+        Main.clickOnElement(By.xpath("/html/body/div/table/tbody/tr[last()]/td[5]/a[1]"));
+        //Suvedame reiksmes i laukus
+        Main.sendKeysToInputField(By.xpath("//*[@id=\"command\"]/p[1]/input"), "-1");
+        //Spaudziame Atnaujinti
+        Main.clickOnElement(By.xpath("//*[@id=\"command\"]/p[5]/input"));
+        //Patikriname gauta klaida
+        boolean errorMsg = Main.elementTextContainsString(By.tagName("h1"), "Whitelabel Error Page");
+        Assert.assertTrue(errorMsg);
+    }
+    @Test (priority = 11)
+    static void trintiIrasaNegatyvusTestas(){
+        Main.browser.get(Main.URL);
+        //Nueiname i irasu registra
+        Main.clickOnElement(By.xpath("/html/body/nav/div/ul[1]/li/a"));
+
+        //Papsaudziame trinti ant paskutinio iraso
+        Main.clickOnElement(By.xpath("/html/body/div/table/tbody/tr[last()]/td[5]/a[2]"));
+        // Pereiname prie alert ir patvirtiname jį
+        Alert alert = Main.browser.switchTo().alert();
+        alert.dismiss();
+        //Patikriname, kad irasas istrintas - neberandame jo registre
+        boolean resultActual = Main.findRecordInTable(1, 991, "-", -990);
+        Assert.assertTrue(resultActual);
+    }
+    @Test (priority = 12)
+    static void trintiIrasa(){
+        Main.clickOnElement(By.xpath("/html/body/div/table/tbody/tr[last()]/td[5]/a[2]"));
+        // Pereiname prie alert ir patvirtiname jį
+        Alert alert = Main.browser.switchTo().alert();
+        alert.accept();
+        //Patikriname, kad irasas istrintas - neberandame jo registre
+        boolean resultActual = Main.findRecordInTable(1, 991, "-", -990);
+        Assert.assertFalse(resultActual);
+    }
+    @Test (priority = 13)
+    static void patikrintiAutorizuotoVartotojoGalimybesMatytiRegistra(){
+        //Nueiname i irasu registra
+        Main.clickOnElement(By.xpath("/html/body/nav/div/ul[1]/li/a"));
+        //Nusikopijuojame jo URL adresa
+        Main.innerUrl = Main.browser.getCurrentUrl();
+        //Atsijungiame
+        Main.clickOnElement(By.xpath("/html/body/nav/div/ul[2]/a"));
+
+        //Prisijungti nauju vartotoju
+        // Padidinti vienu ir išsaugoti skaitiklį counter
+        Main.saveCounter(Main.counter + 1);
+        // Paimti counter reikšmę iš failo ir sukurti unikalu username
+        Main.counter = Main.readCounter();
+        String username = "Testavimas" + Main.counter;
+        //Paspaudžiame "Sukurti naują paskyrą"
+        Main.clickOnElement(By.xpath("/html/body/div/form/div/h4/a"));
+        //Užpildome reikalingus laukus teisingai
+        Main.sendKeysToInputField(By.id("username"), username);
+        Main.sendKeysToInputField(By.id("password"), "testavimas");
+        Main.sendKeysToInputField(By.id("passwordConfirm"), "testavimas");
+        //paspaudziame "Sukurti"
+        Main.clickOnElement(By.xpath("//*[@id=\"userForm\"]/button"));
+        Main.browser.get(Main.innerUrl);
+        String resultActual = Main.browser.getCurrentUrl();
+        Assert.assertEquals(resultActual, Main.innerUrl);
+    }
+    @Test (priority = 14)
+    static void patikrintiAutorizuotoVartotojoGalimybesMatytiRegistraNegatyvusTestas(){
+        //Nueiname i irasu registra
+        Main.clickOnElement(By.xpath("/html/body/nav/div/ul[1]/li/a"));
+        //Nusikopijuojame jo URL adresa
+        Main.innerUrl = Main.browser.getCurrentUrl();
+        Main.clickOnElement(By.xpath("/html/body/nav/div/ul[2]/a"));
+
+        //Su neautorizuotu vetotoju bandeome pasieki vidini psl
+        Main.browser.get(Main.innerUrl);
+        String resultActual = Main.browser.getCurrentUrl();
+        Assert.assertNotEquals(resultActual, Main.innerUrl);
+    }
+    @AfterClass
+    public static void closeBrowserAndIncrementCounter(){
+        // Padidinti vienu ir išsaugoti skaitiklį counter
+        Main.saveCounter(Main.counter + 1);
+        Main.closeBrowser();
+    }
+}
